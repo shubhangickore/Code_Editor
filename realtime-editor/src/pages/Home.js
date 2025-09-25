04cdd0c75da1d8bd0ebe2d08cc1882e2ecabf425
shubhangi kore <koreshubhangi291@gmail.com>
2025-09-25 22:21:37 +0530

    updated files

--- realtime-editor/src/pages/Home.js
+++ realtime-editor/src/pages/Home.js
@@ -2,8 +2,36 @@ import React from  'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1> Home page</h1>
+    <div className ="homePagewrapper">
+     <div className="formwrapper">
+
+      <img src="/code-sync.png" alt='code-sync' height = "300px" width ="300px"/>
+
+      <h4 className='mainLabel'>Paste invitation roomId</h4>
+      <div className= "inputGroup">
+        <input type ="text" 
+        className= "inputBox" 
+        placeholder="Enter Room Id">
+        </input>
+
+         <input type ="text" 
+        className= "inputBox" 
+        placeholder="UserName">
+        </input>
+
+        <button className='btn joinBtn'> Join </button>
+        <span className='createInfo'> 
+          If you dont have an Invite create 
+          <a href= "" className="createNewBtn">New Room</a>
+        </span>
+
+      </div>
+     </div>
+     <footer>
+      <h4> Built with 
+        <a href="https://github.com/shubhangickore">Shubhangi kore</a>
+      </h4>
+     </footer>
     </div>
   )
 }
