56fc56119b84ed2ef02ee333701161cff834c42b
shubhangi kore <koreshubhangi291@gmail.com>
2025-09-27 23:44:43 +0530

    updated files

--- realtime-editor/server.js
+++ realtime-editor/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const {Server} = require (socket.io);
+const { Server } = require('socket.io');
 
+const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 
-io.on('connection',(socket) =>{
-    console.log('socket connected',socket.id);
+const io = new Server(server, {
+  cors: {
+    origin: '*',  // allow your frontend origin
+    methods: ['GET', 'POST']
+  }
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen (PORT ,()=>{
-    console.log("server is listening on ${PORT}");
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
