38cefab143b51564df72ca2bab41a5b5fc45dd97
shubhangickore <koreshubhangi291@gmail.com>
2026-04-11 00:36:43 +0530

    fix socket cors for production

--- realtime-editor/server.js
+++ realtime-editor/server.js
@@ -6,12 +6,7 @@ const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+const io = new Server(server);
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
